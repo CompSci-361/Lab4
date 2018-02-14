a0f74b6cc56e0cc26da0ab3098599f766f56f3a7
cjscholl <catelynjscholl@gmail.com>
2018-02-14 13:40:09 -0600

    Card Class
    Created card constructor and getter method for account number

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+
+public class Card {
+	private int accountNumber;
+	
+	public Card(int accountNumber){
+		this.accountNumber = accountNumber;
+	}
+	
+	public int getAccountNumber(Card card){
+		return this.accountNumber;
+	}
+}
