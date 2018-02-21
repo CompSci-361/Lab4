67ac5770815f0b1876901a272386a4258fb7bb2f
Amrykid <Amrykid@gmail.com>
2018-02-21 16:30:33 -0600

    Renamed Application.java -> Driver.java. Working on merging #16 to current (master).

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
-public class Application {
+public class Driver {
 
 	public static void main(String[] args) throws Throwable {
     	ArrayList<Account> initialAccounts = getInitialAccounts();
